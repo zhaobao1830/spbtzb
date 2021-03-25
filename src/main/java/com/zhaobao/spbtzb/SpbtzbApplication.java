b307fbec61286a0260d6377263bb6fb2ac8702a6
zhaobao1830 <zgt..5219438>
2021-03-25 15:01:41 +0800

    初始化项目

---
+++ src/main/java/com/zhaobao/spbtzb/SpbtzbApplication.java
@@ -0,0 +1,13 @@
+package com.zhaobao.spbtzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtzbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbtzbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhaobao/spbtzb/SpbtzbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaobao.spbtzb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtzbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
