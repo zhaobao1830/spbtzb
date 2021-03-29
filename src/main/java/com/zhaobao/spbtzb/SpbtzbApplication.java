fa7529c32260b7e0440199843180a593eaf1e4b4
zhaobao1830 <zgt..5219438>
2021-03-29 09:01:07 +0800

    优化代码

--- src/main/java/com/zhaobao/spbtzb/SpbtzbApplication.java
+++ src/main/java/com/zhaobao/spbtzb/SpbtzbApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhaobao
+ */
 @SpringBootApplication
 @MapperScan(basePackages = "com.zhaobao.spbtzb.dao")
 public class SpbtzbApplication {
